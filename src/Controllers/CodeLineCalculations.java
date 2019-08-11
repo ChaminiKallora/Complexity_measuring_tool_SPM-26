d4229fb327845dd9f05afd58a639b9f56cbf6639
ChaminiKallora <chaminikallora@gmail.com>
2019-08-11 12:32:26 +0530

    Update 3

--- src/Controllers/CodeLineCalculations.java
+++ src/Controllers/CodeLineCalculations.java
@@ -6,6 +6,11 @@
 package Controllers;
 
 import Interfaces.Calculation;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,54 +27,73 @@ public class CodeLineCalculations {
     String code;
     ArrayList<String> codeLines = new ArrayList<String>();
     ArrayList<String> classExtended = new ArrayList<String>();
+    ArrayList<String> classes = new ArrayList<String>();
     
-    public ArrayList<String> breakLines(String theRegex,String code){
-        this.code = code;
-        Pattern checkRegex = Pattern.compile(theRegex);
-        Matcher regexMatcher = checkRegex.matcher(code);
+    public ArrayList<String> breakLines(String theRegex,String code, File ff) throws FileNotFoundException, IOException{
+//        this.code = code;
+//        Pattern checkRegex = Pattern.compile(theRegex);
+//        Matcher regexMatcher = checkRegex.matcher(code);
+//        
+//        if(".*[{};]".equals(theRegex)){
+//            while(regexMatcher.find()){ 
+//                if(regexMatcher.group().length() != 0){ 
+//                    codeLines.add(regexMatcher.group().trim());
+//                }           
+//    //            System.out.println("Start index : " +regexMatcher.start());
+//    //            System.out.println("End index : " +regexMatcher.end());
+//            }
+//            return codeLines;
+//        }else if("\\w*\\sextends\\s\\w*".equals(theRegex)){
+//            while(regexMatcher.find()){ 
+//                if(regexMatcher.group().length() != 0){ 
+//                    classExtended.add(regexMatcher.group().trim());
+//                }           
+//            }
+//            return classExtended;
+//        }
+
+        FileReader fr = new FileReader(ff);    
+        BufferedReader br=new BufferedReader(fr);    
+  
+        int i;    
+        while((i=br.read())!=-1){  
+            System.out.print((char)i);  
         
-        if(".*[{};]".equals(theRegex)){
-            while(regexMatcher.find()){ 
-                if(regexMatcher.group().length() != 0){ 
-                    codeLines.add(regexMatcher.group().trim());
-                }           
-    //            System.out.println("Start index : " +regexMatcher.start());
-    //            System.out.println("End index : " +regexMatcher.end());
-            }
-            return codeLines;
-        }else if("\\w*\\sextends\\s\\w*".equals(theRegex)){
-            while(regexMatcher.find()){ 
-                if(regexMatcher.group().length() != 0){ 
-                    classExtended.add(regexMatcher.group().trim());
-                }           
-            }
-            return classExtended;
-        }
+        }  
         return null;
     }
     
     public void checkExtends(){
         int i = 0;
-        breakLines("\\w*\\sextends\\s\\w*", code);
+        //breakLines("\\w*\\sextends\\s\\w*", code);
         
-        Map<String,String> classes = new HashMap<String,String>();  
+        Map<String,String> cls = new HashMap<String,String>();  
         
         for (String temp : classExtended) {
             String[] splitArray = temp.split(" extends ", 3);
             System.out.println(splitArray[0]);
             System.out.println(splitArray[1]);
             
-            classes.put(splitArray[0], splitArray[1]);
-            
+            cls.put(splitArray[0], splitArray[1]);
         }
         
-        for(Map.Entry c : classes.entrySet()){  
+        for(Map.Entry c : cls.entrySet()){  
 //            System.out.println(c.getKey()+" "+c.getValue());  
             
         }  
         
     }
     
-    public void findClasses(){}
+    public void findClasses(){
+//        FileReader fr = new FileReader("D:\\testout.txt");    
+//        BufferedReader br=new BufferedReader(fr);    
+//  
+//        int i;    
+//        while((i=br.read())!=-1){  
+//            System.out.print((char)i);  
+//        
+//        }  
+        
+    }
     
 }
